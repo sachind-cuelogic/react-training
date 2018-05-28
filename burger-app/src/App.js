483ccaa233eada6e9e5bef1ce490579d754f162e
Sachin Dane <sachin.dane@cuelogic.com>
2018-05-28 13:30:50 +0530

    complete burger app

--- burger-app/src/App.js
+++ burger-app/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';   
-import Layout from './components/Layout/Layout';
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
         <Layout>
-          <p>Test Burger App</p>
           <BurgerBuilder />
         </Layout>
       </div>
